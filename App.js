8798b71216c14e12ab28b19b7927c617152fbf91
itay-az <65295205+itay-az@users.noreply.github.com>
2022-12-27 17:05:49 +0200

    not saved

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import { styles } from './style/style';
 import MainStack from './src/navigation/index';
 import {NavigationContainer} from '@react-navigation/native';
 
+import Questions from './src/screens/Questions';
+
 
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <MainStack />
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <MainStack />
+    // </NavigationContainer>
+
+    <Questions />
 
   );
 }
--- src/screens/Questions.js
+++ src/screens/Questions.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View,Text} from 'react-native';
 import { styles } from '../../style/style';
 
 const Questions = () => {
+
+    const getData = async() => {
+        const response = await fetch('https://opentdb.com/api.php?amount=20&category=18',{method:'get'});
+        const data = await response.json();
+        
+    };
+
+    useEffect(() => {
+        getData();
+    },[])
 
     return(
         <View style={styles.container}>
             <Text>Questions</Text>
         </View>
     )
+}
+export const screenOptins = () => {
+    return {
+        headerShown:true,
+        
+    }
 }
 
 export default Questions;
